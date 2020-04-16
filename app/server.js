76b147bab5e24b0d60812b20e4e0189ade5ed813
Andy Kuny <andykuny@gmail.com>
2020-04-16 12:59:42 -0500

    Remove old app dir

--- app/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.Server((req, res) => {
-    console.log(`Request for: ${req.url}`);
-    res.statusCode = 200;
-    return res.end('Yo');
-});
-
-module.exports = server;
