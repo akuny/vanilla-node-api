32a1f7a3d69ba2a99559f0d7a98b409541e7b87b
Andy Kuny <andykuny@gmail.com>
2020-04-16 12:56:01 -0500

    Add crude router

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const server = require('./app/server');
+const server = require('./lib/server');
 const port = process.env.PORT;
 
 server.listen(port, 'localhost', () => {
