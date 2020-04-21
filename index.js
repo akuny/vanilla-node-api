778ff8ebaf8cac9b1c9ea2288bb6c0e96a25a83e
Andy Kuny <andykuny@gmail.com>
2020-04-21 12:59:58 -0500

    Add database and model

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const config = require('./lib/config');
-const server = require('./lib/server');
+const db = require('./lib/database');
+const app = require('./lib/app');
 
-server.listen(config.port, 'localhost', () => {
-    console.log(`Server up and running on ${config.port}`); 
+db.connect({ url: config.databaseURL, name: config.databaseName }, (err, isConnected) => {
+    if (err) {
+        console.log(err);
+    }
+    
+    console.log('Connected to database');
+
+    app.listen(config.port, () => {
+        console.log(`Server up and running on ${config.port}`); 
+    });
 });
